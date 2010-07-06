0fc8abb9b5606eaf9b49d03c1d5ce70832cb5275
Felix Schäfer <felix@fachschaften.org>
2010-07-06 14:37:32 +0000

    finer-grained permissions #907

--- init.rb
+++ init.rb
@@ -15,8 +15,10 @@ Redmine::Plugin.register :redmine_doodles do
   version 'trunk'
   
   project_module :doodles do
-    permission :manage_doodles, {:doodles => [:new, :create, :update, :lock]}, :require => :loggedin
+    permission :manage_doodles, {:doodles => [:lock]}, :require => :member
+    permission :create_doodles, {:doodles => [:new, :create]}, :require => :member
     permission :view_doodles, {:doodles => [:index, :show]}
+    permission :answer_doodles, {:doodles => [:update]}, :require => :loggedin
   end
   menu :project_menu, :doodles, { :controller => 'doodles', :action => 'index' }, :caption => :label_doodle_plural, :param => :project_id
 end
